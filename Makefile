8c9bf18aa3a64360ee56819125bd2e47ee6687e0
Brandon Pollack <brandonpollack23@gmail.com>
2017-12-11 18:31:38 -0800

    setup complete

---
+++ Makefile
@@ -0,0 +1,43 @@
+CC = g++
+INCLUDEDIRS = grok3d/Grok3d_Windows/inc grok3d/glm grok3d/notstd
+CFLAGS = -std=c++17 -g -O2 -Wall -fno-exceptions $(GLFAGS) $(addprefix -I, $(INCLUDEDIRS))
+
+LD = g++
+GL_LD_FLAGS = -lglfw -lGL -ldl
+LDFLAGS = $(GL_LD_FLAGS)
+
+TARGET = grok3dtests
+
+SOURCES = $(shell find ./src -name "*.cpp" -o -name "*.c")
+OBJECTS = $(patsubst ./src/%, ./$(OBJDIR)/%, $(patsubst %.c, %.o, $(patsubst %.cpp, %.o, $(SOURCES))))
+
+VPATHCPP = $(shell find ./src -name "*.cpp" | sed 's/\(.*\)\/.*\..*$$/\1/g' | sort -u)
+VPATHOBJ = $(patsubst ./src%, ./$(OBJDIR)%, $(VPATHCPP))
+VPATH = $(VPATHCPP) $(VPATHOBJ)
+
+BINDIR = bin
+OBJDIR = o
+
+all: $(TARGET)
+
+$(TARGET): $(OBJECTS) grok3d/Grok3d_Windows/o/libgrok3d.a | $(BINDIR)
+	$(LD) -o $(BINDIR)/$@ $^ $(LDFLAGS)
+
+$(OBJDIR)/%.o: %.cpp | $(VPATHOBJ)
+	$(CC) -c $(CFLAGS) $< -o $@
+
+grok3d/Grok3d_Windows/o/libgrok3d.a:
+	cd grok3d/Grok3d_Windows && make
+
+$(BINDIR):
+	mkdir -p $@
+
+$(VPATHOBJ):
+	mkdir -p $@
+
+clean:
+	rm -rf o bin
+	cd grok3d/Grok3d_Windows && make clean
+
+print-%: 
+	@echo $($*)
---
+++ src/main.cpp
@@ -0,0 +1,90 @@
+/* Copyright (c) 2017 Brandon Pollack
+* Contact @ grok3d@gmail.com
+* This file is available under the MIT license included in the project
+*/
+#include "grok3d.h"
+
+#include <cmath>
+#include <iostream>
+#include <memory>
+
+using namespace Grok3d;
+using namespace Grok3d::Entities;
+using namespace Grok3d::Components;
+
+class MoveBackAndForthBehavour : public GRK_GameBehaviourBase
+{
+    public:
+        MoveBackAndForthBehavour(GRK_EntityHandle entity) noexcept : 
+            GRK_GameBehaviourBase(entity),
+            m_transform(entity.GetComponent<GRK_TransformComponent>())
+        {
+        }
+
+        auto Update(double dt) -> void override
+        {
+            static const float speed = 1.0f;
+
+            float translationX = m_direction * speed * static_cast<float>(dt);
+
+            m_updateCount++;
+
+            m_transform->TranslateLocal(translationX, 0, 0);
+
+            glm::dvec3 worldPosition = m_transform->GetWorldPosition();
+
+            if (abs(worldPosition.x) > 1 * m_behaviourHandle)
+            {
+                //m_direction = -m_direction;
+                UnregisterThisBehaviour();
+                m_owningEntity.Destroy();
+            }
+
+            if (m_updateCount == 72)
+            { 
+                std::cout << "Entity #" << m_owningEntity << ": " << worldPosition.x << ", " << worldPosition.y << ", " << worldPosition.z << std::endl;
+                m_updateCount = 0;
+            }
+        }
+
+    private:
+        GRK_ComponentHandle<GRK_TransformComponent> m_transform;
+        float m_direction = 1;
+        int m_updateCount = 0;
+};
+
+auto CreateAndRegisterMoveTestEntity(GRK_EntityComponentManager& ecm) -> GRK_Result
+{
+    auto pointEntity = ecm.CreateEntity();
+
+    auto moveBehaviour = std::make_unique<MoveBackAndForthBehavour>(pointEntity);
+
+    GRK_GameLogicComponent glc;
+    glc.RegisterBehaviour(std::move(moveBehaviour));
+
+    return pointEntity.AddComponent(std::move(glc));
+}
+
+auto main() -> int
+{
+    std::cout << "Running Grok3d tests" << std::endl;
+
+    auto engineInitialization = [](GRK_EntityComponentManager& ecm) -> GRK_Result
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            auto result = CreateAndRegisterMoveTestEntity(ecm);
+            if (result != GRK_Result::Ok)
+            {
+                return result;
+            }
+        }
+
+        return GRK_Result::Ok;
+    };
+
+    GRK_Engine engine(engineInitialization);
+    engine.Run();
+
+    return 0;
+}
